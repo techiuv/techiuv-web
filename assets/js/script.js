19b67be45ae23c462718ee8e8fc33f118a291565
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-10-01 20:54:10 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
-document.getElementById("copyright").innerHTML = "&copy;" + new Date().getFullYear() + ". All rights reserved.";
-
+document.onreadystatechange = function () {
+    let progressBar = document.getElementById('progressBar');
+    
+    if (document.readyState === 'loading') {
+        // Simulate the progress bar animation
+        progressBar.style.width = '50%';
+    }
+    
+    if (document.readyState === 'interactive') {
+        progressBar.style.width = '75%';
+    }
+    
+    if (document.readyState === 'complete') {
+        progressBar.style.width = '100%';
+        setTimeout(function () {
+            progressBar.style.display = 'none';
+        }, 500); // delay before hiding
+    }
+};
 
 function highlightWords(paragraphClass, wordsToHighlight) {
     let paragraphs = document.querySelectorAll(paragraphClass);
@@ -41,32 +58,133 @@ calcExp()
 
 highlightWords('.about-p', importantWords);
 
-function loadProjects() {
-    fetch('data/projects.json')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            const projectsContainer = document.getElementById('projects-container');
-            projectsContainer.innerHTML = ''; // Clear existing content
 
-            data.forEach(project => {
-                const projectCard = document.createElement('div');
-                projectCard.className = 'project-card';
-                projectCard.innerHTML = `
-                    <h2>${project.title}</h2>
-                    <img src="${project.image}" alt="${project.title}">
-                    <p>${project.description}</p>
-                    <a href="${project.link}" target="_blank">View Project</a>
-                `;
-                projectsContainer.appendChild(projectCard);
+async function loadProjectsMap() {
+    try {
+        const response = await fetch('./data/projects.json');
+        
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        const projectsMap = new Map();
+
+        data.forEach(project => {
+            projectsMap.set(project.title, {
+                image: project.image,
+                description: project.description,
+                link: project.link
             });
-        })
-        .catch(error => console.error('Error loading projects:', error));
+        });
+
+        // Now, you can access the project data through the projectsMap
+        console.log(projectsMap);
+
+        // For example, iterating over the Map:
+        const projectsContainer = document.getElementById('projects-container');
+        projectsContainer.innerHTML = ''; // Clear existing content
+
+        for (let [title, projectDetails] of projectsMap) {
+            const projectCard = document.createElement('div');
+            projectCard.className = 'project-card';
+            projectCard.innerHTML = `
+                <h2>${title}</h2>
+                <img src="${projectDetails.image}" alt="${title}">
+                <p>${projectDetails.description}</p>
+                <a href="${projectDetails.link}" target="_blank">View Project</a>
+            `;
+            projectsContainer.appendChild(projectCard);
+        }
+
+    } catch (error) {
+        console.error('Error loading projects:', error.message);
+    }
 }
 
-// Call the function to load projects when the DOM is fully loaded
+//loadProjectsMap();
+
+
+// Call the function when the DOM is fully loaded
 // document.addEventListener('DOMContentLoaded', loadProjects);
+
+
+const tl = gsap.timeline();
+
+tl.from([".intro-heading", ".intro-p"], { 
+    y: 15,   
+    opacity: 0, 
+    duration: 0.5, 
+    stagger: 0.5 
+});
+
+
+
+tl.from(".picture", { 
+   // y: 15,   
+    opacity:0,
+    duration: .5,
+
+}); 
+
+gsap.from(" .about-p", {    
+    y: 10,
+    opacity: 0,
+    stagger:.5,
+    duration: .5,
+    scrollTrigger: {
+        trigger: "#about",
+        start: "top 80%",                 
+    }
+}); 
+
+
+
+
+gsap.fromTo("#about h3 ", 
+    {
+        clipPath: "inset(100% 0% 0% 0%)" // Starts with the element fully hidden from the bottom
+    }, 
+    {
+        clipPath: "inset(0% 0% 0% 0%)", // Ends with the element fully visible
+        opacity: 1, 
+        duration: .5,
+        scrollTrigger: {
+            trigger: "#about",
+            start: "top 80%", 
+            toggleActions: "play none none none"
+        }
+    }
+);
+
+gsap.fromTo("#portfolio h3 ", 
+    {
+        clipPath: "inset(100% 0% 0% 0%)" // Starts with the element fully hidden from the bottom
+    }, 
+    {
+        clipPath: "inset(0% 0% 0% 0%)", // Ends with the element fully visible
+        opacity: 1, 
+        duration: .5,
+        scrollTrigger: {
+            trigger: "#portfolio",
+            start: "top 80%", 
+            toggleActions: "play none none none"
+        }
+    }
+);
+
+
+
+
+gsap.from(" #portfolio h3", {    
+    y: 10,
+    opacity: 0,
+    stagger:.5,
+    duration: .5,
+    scrollTrigger: {
+        trigger: "#portfolio",
+        start: "top 80%",                 
+    }
+}); 
+
+
